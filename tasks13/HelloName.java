ac7373544e2c37b38b7da419ae840b2a1b9fd575
Moluram <acreed3003@gmail.com>
2016-12-15 11:51:10 +0300

    All tasks

---
+++ tasks13/HelloName.java
@@ -0,0 +1,13 @@
+package com.moluram.tasks13;
+
+import java.util.Scanner;
+
+/**
+ * Created by Moluram on 14.12.2016.
+ */
+public class HelloName {
+  public static void main(String[] args) {
+    String name = new Scanner(System.in).nextLine();
+    System.out.println("Hello, " + name);
+  }
+}
