3d1d5f64a729ebfaa446be707c37da163cf5d13a
Moluram <acreed3003@gmail.com>
2016-12-15 23:29:15 +0300

    Change func

--- task4/RealRoots.java
+++ task4/RealRoots.java
@@ -1,5 +1,7 @@
 package com.moluram.task4;
 
+import java.util.ArrayList;
+
 /**
  * Created by Moluram on 15.12.2016.
  */
@@ -9,19 +11,35 @@ public class RealRoots {
       int a = Integer.parseInt(args[0]);
       int b = Integer.parseInt(args[1]);
       int c = Integer.parseInt(args[2]);
-      double D = b*b - 4 * a * c;
-      if(D < 0){
-        System.out.println("Doen't have real roots");
-      } else {
-        double x1 = ((-1 * b) + Math.sqrt(D)) / (2 * a);
-        double x2 = ((-1 * b) - Math.sqrt(D)) / (2 * a);
-        System.out.println("x1 = " + x1);
-        System.out.println("x2 = " + x2);
-      }
+      solveEquation(a,b,c);
     }catch (ArrayIndexOutOfBoundsException e){
-      System.out.println("*.class <a> <b> <c>");
+      print("*.class <a> <b> <c>");
     }catch (NumberFormatException e){
-      System.out.println("The number must consist only of digits");
+      print("The number must consist only of digits");
+    }catch (DoesNotHaveRealRoots e){
+      print("Equation doesn't have real roots");
     }
   }
+
+  private static ArrayList<Double> solveEquation(int a, int b, int c) throws DoesNotHaveRealRoots{
+    double D = b*b - 4 * a * c;
+    double x1;
+    double x2;
+    if(D < 0){
+      throw  new DoesNotHaveRealRoots();
+    } else {
+      x1 = ((-1 * b) + Math.sqrt(D)) / (2 * a);
+      x2 = ((-1 * b) - Math.sqrt(D)) / (2 * a);
+    }
+    ArrayList<Double> list = new ArrayList<Double>();
+    list.add(x1);
+    list.add(x2);
+    return list;
+  }
+
+  private static void print(String value){
+    System.out.println(value);
+  }
+
+  private static class DoesNotHaveRealRoots extends Exception{}
 }
