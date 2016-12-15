fa87e3d61511ce5d1577c2f0deeedcf59ebad057
Moluram <acreed3003@gmail.com>
2016-12-16 00:01:37 +0300

    Add comment

--- task4/RealRoots.java
+++ task4/RealRoots.java
@@ -3,15 +3,23 @@ package com.moluram.task4;
 import java.util.ArrayList;
 
 /**
- * Created by Moluram on 15.12.2016.
+ * Class serve for calculating roots from equation
+ * @author Moluram
+ * @version 1.0
  */
 public class RealRoots {
+   /**
+   * Takes 3 parameters from command line and calls for roots of the equation
+   * @param args - attributes from command line
+   */
   public static void main(String[] args) {
     try {
       int a = Integer.parseInt(args[0]);
       int b = Integer.parseInt(args[1]);
       int c = Integer.parseInt(args[2]);
-      solveEquation(a,b,c);
+      for (Double value : solveEquation(a,b,c)) {
+        print(value.toString());
+      }
     }catch (ArrayIndexOutOfBoundsException e){
       print("*.class <a> <b> <c>");
     }catch (NumberFormatException e){
@@ -21,6 +29,15 @@ public class RealRoots {
     }
   }
 
+  /**
+   * Gets real roots from the equation :
+   * a*x^2 + b*x + c = 0
+   * @param a - attribute for equation
+   * @param b - attribute for equation
+   * @param c - attribute for equation
+   * @return array of roots
+   * @throws DoesNotHaveRealRoots
+   */
   private static ArrayList<Double> solveEquation(int a, int b, int c) throws DoesNotHaveRealRoots{
     double D = b*b - 4 * a * c;
     double x1;
@@ -37,9 +54,16 @@ public class RealRoots {
     return list;
   }
 
+  /**
+   * Prints to the console value
+   * @param value - printed value
+   */
   private static void print(String value){
     System.out.println(value);
   }
 
+  /**
+   * Throws when equation doesn't have real roots
+   */
   private static class DoesNotHaveRealRoots extends Exception{}
 }
