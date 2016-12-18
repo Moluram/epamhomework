8991e931d882b8ec1bd6bac002543e011bfc2b39
Moluram <acreed3003@gmail.com>
2016-12-18 22:43:18 +0300

    Add exception for not real triangles

--- task5/Triangle.java
+++ task5/Triangle.java
@@ -18,11 +18,29 @@ public class Triangle {
       BigDecimal a = new BigDecimal(args[0]);
       BigDecimal b = new BigDecimal(args[1]);
       BigDecimal c = new BigDecimal(args[2]);
-      print(whatIsThisTriangle(a,b,c));
+      testTriangle(a, b, c);
+      print(whatIsThisTriangle(a, b, c));
+    } catch (TriangleDoesNotExist e) {
+      print("Triangle does not exist");
     } catch (ArrayIndexOutOfBoundsException e) {
       print("You must enter 3 lengths of the triangle as arguments of the command line");
     } catch (NumberFormatException e) {
       print("The number must consist only of digits");
+    }
+  }
+
+  /**
+   * Determines whether this triangle exist
+   * Throws an exception if not
+   * @param a - length of triangle side
+   * @param b - length of triangle side
+   * @param c - length of triangle side
+   * @throws TriangleDoesNotExist
+   */
+  private static void testTriangle(BigDecimal a, BigDecimal b, BigDecimal c)
+          throws TriangleDoesNotExist {
+    if(a.compareTo(b.add(c)) != -1 || b.compareTo(a.add(c)) != -1 || c.compareTo(a.add(b)) != -1) {
+      throw new TriangleDoesNotExist();
     }
   }
 
@@ -51,5 +69,10 @@ public class Triangle {
   private static void print(String value){
     System.out.println(value);
   }
+
+  /**
+   *  Throws when triangle does not exist
+   */
+  private static class TriangleDoesNotExist extends Exception {}
 }
 
