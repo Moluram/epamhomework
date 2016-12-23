1dda5c7354a1fb6bfb3d380a96a66be7822f2dc7
Moluram <acreed3003@gmail.com>
2016-12-23 14:38:10 +0300

    Add docs for exception

--- task7/checker/NotEnoughWordsException.java
+++ task7/checker/NotEnoughWordsException.java
@@ -1,7 +1,7 @@
 package com.moluram.task7.checker;
 
 /**
- * Created by Moluram on 23.12.2016.
+ * Throws when there are not enough words for test
  */
 public class NotEnoughWordsException extends Exception {
 }
